8f15372d0c03ede5ce8b9460109ba82b6a912d1b
liemander <liemander16@gmail.com>
2023-03-04 17:24:39 +0200

    07 - 02

---
+++ Source/ShootThemUp/Private/Pickups/STUBasePickup.cpp
@@ -0,0 +1,38 @@
+// Shoot Them Up Game. All Rights Reserved
+
+#include "Pickups/STUBasePickup.h"
+#include "Components/SphereComponent.h"
+
+DEFINE_LOG_CATEGORY_STATIC(LogBasePickup, All, All)
+
+ASTUBasePickup::ASTUBasePickup()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	CollisionComponent = CreateDefaultSubobject<USphereComponent>("SphereComponent");
+    CollisionComponent->InitSphereRadius(50.0f);
+    CollisionComponent->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+    CollisionComponent->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Overlap);
+    SetRootComponent(CollisionComponent);
+}
+
+void ASTUBasePickup::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ASTUBasePickup::NotifyActorBeginOverlap(AActor* OtherActor) 
+{
+    Super::NotifyActorBeginOverlap(OtherActor);
+
+	UE_LOG(LogBasePickup, Display, TEXT("Actor begin overlap"));
+    Destroy();
+}
+
+void ASTUBasePickup::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
