2be77397f0e866307a20ad60d0cfaac02fda13e0
liemander <liemander16@gmail.com>
2023-03-05 19:34:26 +0200

    08 - 04

--- Source/ShootThemUp/Private/Weapon/Components/STUWeaponFXComponent.cpp
+++ Source/ShootThemUp/Private/Weapon/Components/STUWeaponFXComponent.cpp
@@ -10,5 +10,18 @@ USTUWeaponFXComponent::USTUWeaponFXComponent()
 
 void USTUWeaponFXComponent::PlayImpactFX(const FHitResult& Hit) 
 {
+    auto Effect = DefaultEffect;
+
+    if (Hit.PhysMaterial.IsValid())
+    {
+        const auto PhysMaterial = Hit.PhysMaterial.Get();
+        if (EffectsMap.Contains(PhysMaterial))
+        {
+            Effect = EffectsMap[PhysMaterial];
+        }
+    }
+    UE_LOG(LogTemp, Display, TEXT("%s"), *Hit.GetActor()->GetName());
+    UE_LOG(LogTemp, Display, TEXT("%s"), *Hit.BoneName.ToString());
+    
     UNiagaraFunctionLibrary::SpawnSystemAtLocation(GetWorld(), Effect, Hit.ImpactPoint, Hit.ImpactNormal.Rotation());
 }
