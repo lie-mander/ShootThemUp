01f672b5bd12adcfea217a6537966a4dbe35494e
liemander <liemander16@gmail.com>
2023-03-14 13:31:47 +0200

    09 - 13

---
+++ Source/ShootThemUp/Public/AI/EQS/STUEnemyEnvQueryContext.h
@@ -0,0 +1,17 @@
+// Shoot Them Up Game. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnvironmentQuery/EnvQueryContext.h"
+#include "STUEnemyEnvQueryContext.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTTHEMUP_API USTUEnemyEnvQueryContext : public UEnvQueryContext
+{
+	GENERATED_BODY()
+	
+};
