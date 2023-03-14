c27ff06900fcb39eb1fdce072cc0ae842bf8c3b6
liemander <liemander16@gmail.com>
2023-03-14 09:49:49 +0200

    09 - 08

---
+++ Source/ShootThemUp/Public/AI/Services/STUFireService.h
@@ -0,0 +1,17 @@
+// Shoot Them Up Game. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTService.h"
+#include "STUFireService.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTTHEMUP_API USTUFireService : public UBTService
+{
+	GENERATED_BODY()
+	
+};
